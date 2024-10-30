d881475c6b009c8ebe3dc8ba674b6f2bc289dfbe
School Lab <lab@fathallaiats.edu>
2024-10-30 08:43:30 +0300

    ipiogr

---
+++ WpfApp10/WpfApp10/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+
+
+namespace WpfApp10
+{ 
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string connectionstring = "Data Source=.,Datadialog=employee,Username=sa,Password=FIATS@2024";
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string x = "SELECT COUNT(*) FROM employees WHERE employ_name=@employ_name AND employ_password=@password";
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                connection.Open();
+                string a=username.Text;
+                string b=password.Text;
+                //SqlCommand cmd = new SqlCommand(x,connection);
+                using (SqlCommand cmd = new SqlCommand( x, connection) )
+                {
+                    cmd.Parameters.AddWithValue("@employ_name", a);
+                    cmd.Parameters.AddWithValue("@employ_password", b);
+                    cmd.ExecuteNonQuery(); 
+                }
+                Window window = new Window();
+                window.Show();
+                this.Close();
+
+
+
+            }
+
+        }
+
+        
+
+    }
+}
---
+++ WpfApp10/WpfApp10/Window1.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp10
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        string connectionstring = "Data Source'.',Datadialog='employee',Username='sa',Password=FIATS@2024";
+        public Window1()
+        {
+            InitializeComponent();
+            MainWindow window = new MainWindow();
+            window.Show();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+
+
+
+        }
+
+        private void Load()
+        {
+            using (SqlConnection con = new SqlConnection(connectionstring))
+            {
+                con.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter("selcet *from employees",con);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                e.ItemsSource = dt.DefaultView;
+
+            }
+
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string query = "Inset Into employees (name,password) values (@name,@password)";
+            using(SqlConnection con = new SqlConnection(connectionstring))
+            {
+                SqlCommand cmd=new SqlCommand(query,con);
+               
+
+
+
+            }
+        }
+    }
+}
